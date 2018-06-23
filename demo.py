7330944a90667bc3fa2e95e99934c899abb7c298
q0u04bj3 <q0u04bj3@q0u04bj3de-MacBook-Pro.local>
2018-06-24 07:47:45 +0800

    filter by table elements

--- demo.py
+++ demo.py
@@ -1,30 +1,67 @@
-import unittest, time, os
+# -*- coding: UTF-8 -*-
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import unittest, time, os
 
-class PythonOrgSearch(unittest.TestCase):
-
+class KKBOXTestCase(unittest.TestCase):
     def setUp(self):
-        PROJECT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+    	PROJECT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
         CHROMEDRIVER = os.path.join(PROJECT_DIR, 'chromedriver')
         self.driver = webdriver.Chrome(CHROMEDRIVER)
+        self.driver.get("https://play.kkbox.com")
+        # login
+        self.driver.find_element_by_id("uid").send_keys("*********@kkbox.com")
+        self.driver.find_element_by_id("pwd").send_keys("*********")
+        self.driver.find_element_by_id("login-btn").send_keys(Keys.ENTER)
+        time.sleep(2)
 
-    def test_search_in_python_org(self):
+    """def test_search_artist(self):
         driver = self.driver
-        driver.get("http://www.python.org")
-        self.assertIn("Python", driver.title)
-        # wait 3 seconds
-        time.sleep(3) 
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("pycon")
-        time.sleep(3)
-        elem.send_keys(Keys.RETURN)
-        time.sleep(3)
-        assert "No results found." not in driver.page_source
+        driver.find_element_by_tag_name("input").send_keys(u"韋禮安")
+        time.sleep(1)
+        driver.find_element_by_tag_name("input").send_keys(Keys.ENTER)
+        time.sleep(2)
+        search_result = driver.find_elements_by_css_selector("ul.cards.artists")
+        time.sleep(1)
+        for string in search_result:
+            assert u"韋禮安+郭靜 (Weibird Wei+Claire Kuo)" in string.text"""
 
 
-    def tearDown(self):
-        self.driver.close()
+    def test_search_song(self):
+        driver = self.driver
+        driver.find_element_by_tag_name("input").send_keys(u"因為愛")
+        time.sleep(1)
+        driver.find_element_by_tag_name("input").send_keys(Keys.ENTER)
+        time.sleep(3)
+        #search_result = driver.find_elements_by_css_selector("div.normal")
+
+        #search_result = driver.find_elements_by_xpath("//td[@ng-bind='::song.song_name']")
+        #search_result = driver.find_elements_by_css_selector("div.songs-table>div.normal>td.ng-bind")
+        #time.sleep(1)
+        """for string in search_result:
+            print string.text"""
+
+        song_table = self.driver.find_element_by_tag_name('table')
+        rows = song_table.find_elements_by_tag_name('tr')
+        for row in rows[1:-1]:       
+            col = row.find_elements_by_tag_name('td')[1]
+            #print col.text
+            assert u"因為愛" in col.text
+
+
+    '''def test_search_song_list(self):
+        driver = self.driver
+        driver.find_element_by_tag_name("input").send_keys(u"韋禮安 (William Wei) 歷年精選")
+        time.sleep(1)
+        driver.find_element_by_tag_name("input").send_keys(Keys.ENTER)
+        time.sleep(2)
+        search_result = driver.find_elements_by_css_selector("ol.cards")
+        time.sleep(1)
+        for string in search_result:
+            print string.text
+            assert u"韋禮安 (William Wei) 歷年精選" in string.text'''
+
 
 if __name__ == "__main__":
     unittest.main()
